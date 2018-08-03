5add74a7d6621c84d70b84944c51355311bc5e08
Andre Antunes <andrepalhares1989@gmail.com>
2018-08-02 21:49:28 -0300

    Start to build event handlers

--- app.js
+++ app.js
@@ -1,26 +1,63 @@
+// BUDGET CONTROLLER
 var budgetController = (function(){
 
-    var x = 23;
-
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
+    //  Some code
 
 })();
 
+
+//UI CONTROLLER
 var UIController = (function() {
 
     // Some code
 
 })();
 
+// GLOBAL APP CONTROLLER    
 var controller = (function(budgetCtrl, UIctrl) {
 
-    
+    var ctrlAddItem =  function() {
+        // 1. get the field input data
+        // 2. Add the item to the budget controller
+        // 3. add the item to the UI
+        // 4. Calculate the budget
+        // 5. Display the budget on the UI    
+        console.log('Test 1');     
+    }
+
+    document.querySelector(".add__btn").addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        
+        if(event.keyCode == 13 || event.which === 13) {
+            event.preventDefault();
+            ctrlAddItem();
+        }
+
+    })
+
 })(budgetController, UIController)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
