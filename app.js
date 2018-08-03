2baafaa80ab0a8f4806a4ab55b848c4fe6d43303
Andre Antunes <andrepalhares1989@gmail.com>
2018-08-03 10:50:03 -0300

    Get input values

--- app.js
+++ app.js
@@ -9,23 +9,51 @@ var budgetController = (function(){
 //UI CONTROLLER
 var UIController = (function() {
 
-    // Some code
+    var DOMstrings = {
+        // CREATE A OBJECT TO ORGANIZE THE STRINGS IN THE SELECTORS AND OTHER FUNCTIONS
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+    }
+
+    return {
+        getInput :function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp 
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };   
+        },
+        //RETURNED THE OBJECT TO THE GLOBAL SCOPE USING A CLOSURE SO THE CONTROLLER HAVE ACESS TO THE STRINGS, AND CAN ALSO USE IT IN THE SELECTORS
+        getDOMstrings : function() {
+            return DOMstrings;
+        }
+    };
 
 })();
 
+
+
+
+
 // GLOBAL APP CONTROLLER    
-var controller = (function(budgetCtrl, UIctrl) {
+var controller = (function(budgetCtrl, UICtrl) {
+    // CALL THE OBJECT FROM THE UICONTROLLER (USING THE CLOSURE PROVIDED), AND NOW WE CAN ADD THE STRINGS INSIDE THE DOMstrings OBJECT, PUT IT ALL TOGETHER AND CALL IT INSIDE THIS AND OTHER FUNCTIONS
+    var DOM = UICtrl.getDOMstrings();
+
 
     var ctrlAddItem =  function() {
         // 1. get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
         // 2. Add the item to the budget controller
         // 3. add the item to the UI
         // 4. Calculate the budget
-        // 5. Display the budget on the UI    
-        console.log('Test 1');     
+        // 5. Display the budget on the UI     
     }
 
-    document.querySelector(".add__btn").addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
         
@@ -33,6 +61,8 @@ var controller = (function(budgetCtrl, UIctrl) {
             event.preventDefault();
             ctrlAddItem();
         }
+
+
 
     })
 
