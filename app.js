9cab4868cca488805d91fc16607fe2653be34d98
Andre Antunes <andrepalhares1989@gmail.com>
2018-08-10 20:25:39 -0300

    Create function constructors

--- app.js
+++ app.js
@@ -1,30 +1,54 @@
 // BUDGET CONTROLLER
 var budgetController = (function(){
 
-    //  Some code
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
+
 
 })();
 
 
+
 //UI CONTROLLER
-var UIController = (function() {
+var UIController = (function() { 
 
     var DOMstrings = {
-        // CREATE A OBJECT TO ORGANIZE THE STRINGS IN THE SELECTORS AND OTHER FUNCTIONS
+        // This objects was created to hold strings and selectors in order to make our life easyer in the future if we need to change a ev.handler or else. Since we've created this data we'll only need to make changes in one place.
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
         inputBtn: '.add__btn',
-    }
+    };
 
     return {
         getInput :function() {
             return {
-                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp 
+                type: document.querySelector(DOMstrings.inputType).value, //the .value wfill be either inc or exp 
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value
             };   
         },
+       
         //RETURNED THE OBJECT TO THE GLOBAL SCOPE USING A CLOSURE SO THE CONTROLLER HAVE ACESS TO THE STRINGS, AND CAN ALSO USE IT IN THE SELECTORS
         getDOMstrings : function() {
             return DOMstrings;
@@ -44,11 +68,13 @@ var controller = (function(budgetCtrl, UICtrl) {
     var setupEventListeners = function() {
         // CALL THE OBJECT FROM THE UICONTROLLER (USING THE CLOSURE PROVIDED), AND NOW WE CAN ADD THE STRINGS INSIDE THE DOMstrings OBJECT, PUT IT ALL TOGETHER AND CALL IT INSIDE THIS AND OTHER FUNCTIONS
         var DOM = UICtrl.getDOMstrings();
-
+        // Now the "DOMstrings" from the UIcontroller is called just "DOM", so it's called in the ev.handler bellow as it.
         document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
+        // In the event handler below, we did not look for a selector, because since it's a keypress event it will happend when the WHOLE WINDOW is activated, so no selector needed.
         document.addEventListener('keypress', function(event) {
             
+            // The property ".which" wass used here because, some older browsers, doesn't have support for the  "keyCode" porperty.
             if(event.keyCode == 13 || event.which === 13) {
                 event.preventDefault();
                 ctrlAddItem();
@@ -60,13 +86,13 @@ var controller = (function(budgetCtrl, UICtrl) {
     var ctrlAddItem =  function() {
         // 1. get the field input data
         var input = UICtrl.getInput();
-        console.log(input);
+        console.log(input); // ERASE IT LATTER!! 
 
         // 2. Add the item to the budget controller
         // 3. add the item to the UI
         // 4. Calculate the budget
         // 5. Display the budget on the UI     
-    }
+    };
 
     return {
         init: function() {
