01b54f00065f126a4fac58aa4b2cccbf264e2c97
Andre Antunes <andrepalhares1989@gmail.com>
2018-08-18 21:00:30 -0300

    Fina UX improvments

--- app.js
+++ app.js
@@ -305,6 +305,25 @@ var UIController = (function() {
             document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year ; // Changes the UI
         },
 
+        changedType: function() {
+            var fields = document.querySelectorAll(
+            DOMstrings.inputType + ',' + 
+            DOMstrings.inputDescription + ',' + 
+            DOMstrings.inputValue);
+
+            var nodeListForEach = function(list, callback) {
+                for (var i = 0; i < list.length; i++) {
+                    callback(list[i], i);
+                }
+            }
+
+            nodeListForEach(fields, function(cur) {
+                cur.classList.toggle('red-focus');
+            });
+
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+        },
+
 
         //RETURNED THE OBJECT TO THE GLOBAL SCOPE USING A CLOSURE SO THE CONTROLLER HAVE ACESS TO THE STRINGS, AND CAN ALSO USE IT IN THE SELECTORS
         getDOMstrings : function() {
@@ -341,6 +360,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
         
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
     }
 
     var updateBudget = function() {
